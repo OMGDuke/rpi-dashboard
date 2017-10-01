2aaea5aad8e8fff7288d493b475e927e4c5641ab
OMGDuke <lex1989@gmail.com>
2017-10-01 18:23:39 +0100

    added a weather widget

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,16 +1,30 @@
 <template>
   <div class="dashboard">
   Hello {{username}}
+  <weather 
+        :api-key="this.$env.get('DARKSKY_API_KEY')"
+        title="Weather For Pinner, UK"
+        :latitude="this.$env.get('LAT')"
+        :longitude="this.$env.get('LONG')"
+        language="en"
+        units="uk">
+    </weather>
   </div>
 </template>
 
 <script>
+import VueWeatherWidget from 'vue-weather-widget';
+import 'vue-weather-widget/dist/css/vue-weather-widget.css';
+
 export default {
   name: 'dashboard',
   data() {
     return {
       username: this.$env.get('USERNAME'),
     };
+  },
+  components: {
+    weather: VueWeatherWidget,
   },
 };
 </script>
