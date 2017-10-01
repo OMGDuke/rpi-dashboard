ccea30d7aae8b419a53b9a29f083a1fbb9b17749
OMGDuke <lex1989@gmail.com>
2017-10-01 15:23:43 +0100

    Changed from react to vue

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="dashboard">
+  Hello {{username}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'dashboard',
+  data() {
+    return {
+      username: this.$env.get('USERNAME'),
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
